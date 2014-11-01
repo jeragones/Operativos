cd110c91c87dc02134e1968afd6395700dcf633b
Daniel Berrocal <jdanielb1992@gmail.com>
2014-11-01 14:43:21 -0600

    C# IronMQ

---
+++ Proyecto 3/ejemplo/ironmq/ironmq/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ironmq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cola = new System.Windows.Forms.Label();
+            this.mens = new System.Windows.Forms.TextBox();
+            this.inbox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cant = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mensaje";
+            // 
+            // cola
+            // 
+            this.cola.AutoSize = true;
+            this.cola.Location = new System.Drawing.Point(12, 111);
+            this.cola.Name = "cola";
+            this.cola.Size = new System.Drawing.Size(40, 13);
+            this.cola.TabIndex = 1;
+            this.cola.Text = "Cola: 0";
+            // 
+            // mens
+            // 
+            this.mens.Location = new System.Drawing.Point(12, 36);
+            this.mens.Name = "mens";
+            this.mens.Size = new System.Drawing.Size(292, 20);
+            this.mens.TabIndex = 2;
+            // 
+            // inbox
+            // 
+            this.inbox.Location = new System.Drawing.Point(12, 130);
+            this.inbox.Name = "inbox";
+            this.inbox.Size = new System.Drawing.Size(292, 165);
+            this.inbox.TabIndex = 3;
+            this.inbox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cantidad de Mensajes";
+            // 
+            // cant
+            // 
+            this.cant.Location = new System.Drawing.Point(12, 75);
+            this.cant.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.cant.Name = "cant";
+            this.cant.Size = new System.Drawing.Size(292, 20);
+            this.cant.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cargar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 308);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cant);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inbox);
+            this.Controls.Add(this.mens);
+            this.Controls.Add(this.cola);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cola;
+        private System.Windows.Forms.TextBox mens;
+        private System.Windows.Forms.RichTextBox inbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown cant;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
